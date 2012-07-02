69af23c304e4561f0f3eb9480b78a88728b4e337
Carles F. Julià <carles.fernandez@upf.edu>
2012-07-02 16:51:04 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='ClassLogger',
+    version='0.1dev',
+    author='Carles F. Julià',
+    author_email='carles.fernandez@upf.edu',
+    py_modules=["classlogger"],
+    #url='http://pypi.python.org/pypi/TowelStuff/',
+    #license='LICENSE.txt',
+    description='A Class Logger, with per-class log files',
+    #long_description=open('README.txt').read(),
+)
